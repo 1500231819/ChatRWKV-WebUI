d158d5d082c5f9ec4ed91fd4e5dfff215cfec195
Ceyase <jjcyf@foxmail.com>
2023-02-11 18:06:28 +0800

    Add basic UI

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 ########################################################################################################
 # The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
 ########################################################################################################
-
+import gradio as gr
 import os, copy, types, gc, sys
 import numpy as np
 try:
@@ -11,40 +11,71 @@ except:
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 args = types.SimpleNamespace()
 
-print('\n\nChatRWKV project: https://github.com/BlinkDL/ChatRWKV')
-
 ########################################################################################################
 
-args.RUN_DEVICE = "cuda"  # cuda // cpu
-# fp16 (good for GPU, does NOT support CPU) // fp32 (good for CPU) // bf16 (worse accuracy, supports CPU)
-args.FLOAT_MODE = "fp16"
+# 选择语言
+print('\nPlease choose the language.')
+
+language_list = [('English','English'),('Chinese','简体中文')]
+
+for i in range(len(language_list)):
+    print(f"{i} = {language_list[i][1]}")
+
+CHAT_LANG = language_list[int(input("Waiting for the language ref (etc. 0): "))][0]
+
+# 选择模式
+print('\nPlease choose the mode.')
+
+mode_list = [('cuda','GPU'),('cpu','CPU')]
+
+for i in range(len(mode_list)):
+    print(f"{i} = {mode_list[i][1]}")
+
+_temp_mode = mode_list[int(input("Waiting for the mode ref (etc. 0): "))][0]
+
+if(_temp_mode == "cuda"):
+    args.RUN_DEVICE = "cuda"
+    args.FLOAT_MODE = "fp16"
+
+if(_temp_mode == "cpu"):
+    args.RUN_DEVICE = "cpu"
+    args.FLOAT_MODE = "fp32"
 
 os.environ["RWKV_JIT_ON"] = '1' # '1' or '0'. very useful for fp32, but might be harmful for GPU fp16. please benchmark !!!
-
-CHAT_LANG = 'English' # English // Chinese // more to come
 
 QA_PROMPT = False # True: Q & A prompt // False: User & Bot prompt
 # 中文问答设置QA_PROMPT=True（只能问答，问答效果更好，但不能闲聊） 中文聊天设置QA_PROMPT=False（可以闲聊，但需要大模型才适合闲聊）
 
 # Download RWKV-4 models from https://huggingface.co/BlinkDL
 
-if CHAT_LANG == 'English':
-    args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-14b/RWKV-4-Pile-14B-20230204-7324'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-7b/RWKV-4-Pile-7B-20221115-8047'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-3b/RWKV-4-Pile-3B-20221110-ctx4096'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-3b/RWKV-4-Pile-3B-Instruct-test1-20230124'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-1b5/RWKV-4-Pile-1B5-20220903-8040'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-430m/RWKV-4-Pile-430M-20220808-8066'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-169m/RWKV-4-Pile-169M-20220807-8023'
-    # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/7-run1z/rwkv-340'
-    # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/14b-run1/rwkv-6210'
+# 选择模型
+print('\nPlease choose the model.')
 
-elif CHAT_LANG == 'Chinese':
-    args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-7b/RWKV-4-Pile-7B-EngChn-test4-20230116'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-3b/RWKV-4-Pile-3B-EngChn-test4-20230115'
-    # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-1b5/RWKV-4-Pile-1B5-EngChn-test4-20230115'
-    # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/7-run1z/rwkv-490'
-    # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/1.5-run1z/rwkv-415'
+models_list = os.listdir(r'./models/')
+
+for i in range(len(models_list)):
+    print(f"{i} = {models_list[i]}")
+
+model_num = int(input("Waiting for the model ref (etc. 0): "))
+args.MODEL_NAME = f"./models/{os.path.splitext(models_list[model_num])[0]}"
+
+# if CHAT_LANG == 'English':
+#     args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-14b/RWKV-4-Pile-14B-20230204-7324'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-7b/RWKV-4-Pile-7B-20221115-8047'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-3b/RWKV-4-Pile-3B-20221110-ctx4096'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-3b/RWKV-4-Pile-3B-Instruct-test1-20230124'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-1b5/RWKV-4-Pile-1B5-20220903-8040'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-430m/RWKV-4-Pile-430M-20220808-8066'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-169m/RWKV-4-Pile-169M-20220807-8023'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/7-run1z/rwkv-340'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/14b-run1/rwkv-6210'
+
+# elif CHAT_LANG == 'Chinese':
+#     args.MODEL_NAME = '/models/RWKV-4-Pile-3B-Instruct-test2-20230209'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-3b/RWKV-4-Pile-3B-EngChn-test4-20230115'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-1b5/RWKV-4-Pile-1B5-EngChn-test4-20230115'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/7-run1z/rwkv-490'
+#     # args.MODEL_NAME = '/fsx/BlinkDL/CODE/_PUBLIC_/RWKV-LM/RWKV-v4neo/1.5-run1z/rwkv-415'
 
 args.ctx_len = 1024
 
@@ -124,8 +155,6 @@ say something --> chat with bot. use \\n for new line.
 
 Now talk with the bot and enjoy. Remember to +reset periodically to clean up the bot's memory. Use RWKV-4 14B for best results.
 This is not instruct-tuned for conversation yet, so don't expect good quality. Better use +gen for free generation.
-
-Prompt is VERY important. Try all prompts on https://github.com/BlinkDL/ChatRWKV first.
 '''
 elif CHAT_LANG == 'Chinese':
     interface = ":"
@@ -165,23 +194,9 @@ The following is a verbose and detailed conversation between an AI assistant cal
 +++ --> 继续 +gen / +qa / +qq 的回答
 ++ --> 换个 +gen / +qa / +qq 的回答
 
-作者：彭博 请关注我的知乎: https://zhuanlan.zhihu.com/p/603840957
-
-如果喜欢，欢迎看我们的优质护眼灯: https://withablink.taobao.com
-
 现在可以输入内容和机器人聊天（注意它不大懂中文，它更懂英文）。请经常使用 +reset 重置机器人记忆。
 目前没有“重复惩罚”，所以机器人有时会重复，此时必须使用 + 换成正常回答，以免污染电脑记忆。
 注意：和上下文无关的独立问题，必须用 +qa 或 +qq 问，以免污染电脑记忆。
-
-请先试下列咒语，理解咒语的写法！咒语至关重要。
-+gen \\n活动出席发言稿：\\n大家好，
-+gen \\n怎样创立一家快速盈利的AI公司：\\n1.
-+gen 二向箔是一种超级武器，它的原理是
-+gen \\nimport torch
-【下面这些多试几次】
-+qq 请以《我的驴》为题写一篇作文
-+qq 请以《企鹅》为题写一首诗歌
-+qq 请设定一个奇幻世界，告诉我详细的世界设定。
 '''
 
 # Load Model
@@ -225,6 +240,14 @@ def save_all_stat(srv, name, last_out):
     all_state[n]['rnn'] = copy.deepcopy(model_state)
     all_state[n]['token'] = copy.deepcopy(model_tokens)
 
+local_state = {}
+def save_local_stat(srv, name, last_out):
+    n = f'{name}_{srv}'
+    local_state[n] = {}
+    local_state[n]['out'] = last_out
+    local_state[n]['rnn'] = copy.deepcopy(model_state)
+    local_state[n]['token'] = copy.deepcopy(model_tokens)
+
 def load_all_stat(srv, name):
     global model_tokens, model_state
     n = f'{name}_{srv}'
@@ -238,22 +261,26 @@ def load_all_stat(srv, name):
 print(f'\nRun prompt...')
 
 out = run_rnn(tokenizer.encode(init_prompt))
-save_all_stat('', 'chat_init', out)
+save_local_stat('', 'chat_init', out)
 gc.collect()
 torch.cuda.empty_cache()
 
 srv_list = ['dummy_server']
 for s in srv_list:
-    save_all_stat(s, 'chat', out)
+    save_local_stat(s, 'chat', out)
 
 print(f'### prompt ###\n[{tokenizer.decode(model_tokens)}]\n')
 
+
 def reply_msg(msg):
+    outtext = ""
     print(f'{bot}{interface} {msg}\n')
 
-def on_message(message):
-    global model_tokens, model_state
-
+def on_message(message, state, turbostats):
+    print(f'{user}{interface} {message}')
+    global model_tokens, model_state, all_state
+    all_state = state
+    outtext = ""
     srv = 'dummy_server'
 
     msg = message.replace('\\n','\n').strip()
@@ -282,7 +309,7 @@ def on_message(message):
         out = load_all_stat('', 'chat_init')
         save_all_stat(srv, 'chat', out)
         reply_msg("Chat reset.")
-        return
+        return turbostats,state,turbostats
 
     elif msg[:5].lower() == '+gen ' or msg[:4].lower() == '+qa ' or msg[:4].lower() == '+qq ' or msg.lower() == '+++' or msg.lower() == '++':
 
@@ -342,10 +369,14 @@ def on_message(message):
             
             xxx = tokenizer.decode(model_tokens[out_last:])
             if '\ufffd' not in xxx: # avoid utf-8 display issues
+                outtext += xxx
                 print(xxx, end='', flush=True)
                 out_last = begin + i + 1
                 if i >= FREE_GEN_LEN:
-                    break
+                    turbostats.append((msg,outtext))
+                    state = all_state
+                    return turbostats,state,turbostats
+        outtext += '\n'
         print('\n')
         # send_msg = tokenizer.decode(model_tokens[begin:]).strip()
         # print(f'### send ###\n[{send_msg}]')
@@ -368,8 +399,9 @@ def on_message(message):
         begin = len(model_tokens)
         out_last = begin
         print(f'{bot}{interface}', end='', flush=True)
-        for i in range(999):
-            if i <= 0:
+        i = 0
+        while True:
+            if i == 0:
                 newline_adj = -999999999
             elif i <= CHAT_LEN_SHORT:
                 newline_adj = (i - CHAT_LEN_SHORT) / 10
@@ -388,34 +420,37 @@ def on_message(message):
 
             xxx = tokenizer.decode(model_tokens[out_last:])
             if '\ufffd' not in xxx: # avoid utf-8 display issues
+                outtext += xxx
                 print(xxx, end='', flush=True)
                 out_last = begin + i + 1
             
             send_msg = tokenizer.decode(model_tokens[begin:])
             if '\n\n' in send_msg:
                 send_msg = send_msg.strip()
-                break
-            
-            # send_msg = tokenizer.decode(model_tokens[begin:]).strip()
-            # if send_msg.endswith(f'{user}{interface}'): # warning: needs to fix state too !!!
-            #     send_msg = send_msg[:-len(f'{user}{interface}')].strip()
-            #     break
-            # if send_msg.endswith(f'{bot}{interface}'):
-            #     send_msg = send_msg[:-len(f'{bot}{interface}')].strip()
-            #     break
-
-        # print(f'{model_tokens}')
-        # print(f'[{tokenizer.decode(model_tokens)}]')
-
-        # print(f'### send ###\n[{send_msg}]')
-        # reply_msg(send_msg)
+                turbostats.append((msg,outtext))
+                state = all_state
+                return turbostats,state,turbostats
+            i += 1
         save_all_stat(srv, 'chat', out)
 
-print(HELP_MSG)
+# print(HELP_MSG)
+with gr.Blocks() as demo:
+    gr.Markdown(
+    """
+    # ChatRWKV
+    玩的开心！
+    """)
+    chatbot = gr.Chatbot()
+    state = gr.State(local_state)
+    turbostats = gr.State([])
+    with gr.Row():
+        txt = gr.Textbox(show_label=False, placeholder="输入后回车").style(container=False)
+    txt.submit(on_message, [txt,state,turbostats], [chatbot,state,turbostats])
 
-while True:
-    msg = input(f'{user}{interface} ')
-    if len(msg.strip()) > 0:
-        on_message(msg)
-    else:
-        print('Error: please say something')
+demo.launch(share=True)
+# while True:
+#     msg = input(f'{user}{interface} ')
+#     if len(msg.strip()) > 0:
+#         on_message(msg)
+#     else:
+#         print('Error: please say something')
